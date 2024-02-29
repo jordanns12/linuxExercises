894eba9101e3e73c4c2110ba89a1a5fb1779f889
Yifan Zhang <zhang2873@wisc.edu>
2024-02-29 13:53:45 -0600

    make 5 directory each 4 files,i lines of i

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+mkdir five
+for i in {1..5};do
+    mkdir "five/dir$i"
+    for j in {1..4};do
+	file="five/dir$i/file$j"
+	for k in $(seq 1 $j);do
+	    echo "$j" >> $file
+	done
+    done
+done
+
+	     
