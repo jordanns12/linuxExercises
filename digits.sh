bdb61d141d347ce255bdb016a9eae512d7bb97d7
Yifan Zhang <zhang2873@wisc.edu>
2024-02-29 12:09:58 -0600

    sum 1000-2000 with digits only {0,1}

--- digits.sh
+++ digits.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+sum=0
+
+for i in {1000..2000}; do
+    if [[ $i=~^[01]+$ ]]; then
+        sum=$((sum+i))
+    fi
+done
+
+echo "The sum of numbers between 1000 and 2000 having digits only from {0,1} is: $sum"
+
